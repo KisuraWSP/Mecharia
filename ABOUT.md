ab15fdde9f21e6ba892255533ffd860158677df1
Kisura <85266425+KisuraWSP@users.noreply.github.com>
2026-04-13 22:08:59 +0530

    first commit

---
+++ ABOUT.md
@@ -0,0 +1,16 @@
+# Mecharia
+- This game is a platformer about a samurai character 
+  who is living in a futuristic world of machines
+- he must fight and help machines in this world to survive
+
+# Features
+- Platformer/Sidescroller
+- Automation Game
+- NPCS
+- Engaging Combat
+
+# Must Add
+- A simple Story
+- Sound
+- Proper Options Menu
+- Pause Menu
---
+++ RaylibWasm/Application.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using Raylib_cs;
+using RayGUI;
+
+namespace RaylibWasm
+{
+    public partial class Application
+    {
+        public static float x = 0.5f;
+        private static Texture2D logo;
+        
+        /// <summary>
+        /// Application entry point
+        /// </summary>
+        public static void Main()
+        {
+            Raylib.InitWindow(512, 512, "RaylibWasm");
+            Raylib.SetTargetFPS(60);
+            
+            logo = Raylib.LoadTexture("Resources/raylib_logo.png");
+        }
+
+        /// <summary>
+        /// Updates frame
+        /// </summary>
+        [JSExport]
+        public static void UpdateFrame()
+        {
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawFPS(4, 4);
+            Raylib.DrawText("All systems operational!", 4, 32, 20, Color.Maroon);
+            RayGui.GuiButton(new(100, 100, 100, 100), "hallo");
+            
+            RayGui.GuiSlider(new(100, 200, 100, 100), "min", "max", ref x, 0, 1); 
+            
+            Raylib.DrawTexture(logo, 4, 64, Color.White);
+
+            Raylib.EndDrawing();
+        }
+    }
+}
